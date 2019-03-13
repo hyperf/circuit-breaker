8e352eadff77389040e3dc5f728ba77dde63ea85
李铭昕 <715557344@qq.com>
2019-03-13 10:14:42 +0800

    Rename breaker to circuit-breaker.

---
+++ src/Handler/HandlerInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.org
+ * @document https://wiki.hyperf.org
+ * @contact  group@hyperf.org
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\CircuitBreaker\Handler;
+
+use Hyperf\CircuitBreaker\Annotation\Breaker;
+use Hyperf\Di\Aop\ProceedingJoinPoint;
+
+interface HandlerInterface
+{
+    public function handle(ProceedingJoinPoint $proceedingJoinPoint, Breaker $annotation);
+}
